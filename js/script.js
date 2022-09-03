1438f184db69b4a8c2b80e069e20d00aa37cff26
serhii-sultanov <sshsultanov@gmail.com>
2022-09-03 17:29:24 +0300

    renamed parametres of isAfter function

--- js/script.js
+++ js/script.js
@@ -14,31 +14,31 @@ if (storedUser) {
     .catch((error) => console.error(error));
 }
 
-// // 2
-// function isValidDateFormat(str) {
-//   const regexp = /^\d{4}\/\d{2}\/\d{2} \d{2}\:\d{2}$/;
+// 2
+function isValidDateFormat(str) {
+  const regexp = /^\d{4}\/\d{2}\/\d{2} \d{2}\:\d{2}$/;
 
-//   return regexp.test(str);
-// }
+  return regexp.test(str);
+}
 
-// console.log(isValidDateFormat("2012/09/18 12:10")); // true
-// console.log(isValidDateFormat("12.02.2022 12:10")); // false
+console.log(isValidDateFormat("2012/09/18 12:10")); // true
+console.log(isValidDateFormat("12.02.2022 12:10")); // false
 
-// // 3
-// function isAfter(firstDate, secondDate) {
-//   const enteredFirstDate = new Date(firstDate);
-//   const enteredSecondDate = new Date(secondDate);
+// 3
+function isAfter(date, dateToCompare) {
+  const enteredDate = new Date(date);
+  const enteredDateToCompare = new Date(dateToCompare);
 
-//   if (
-//     enteredFirstDate.toString() === "Invalid Date" ||
-//     enteredSecondDate.toString() === "Invalid Date"
-//   ) {
-//     throw Error("Please, enter a valid date");
-//   }
-//   return enteredFirstDate > enteredSecondDate;
-// }
+  if (
+    enteredDate.toString() === "Invalid Date" ||
+    enteredDateToCompare.toString() === "Invalid Date"
+  ) {
+    throw Error("Please, enter a valid date");
+  }
+  return enteredDate > enteredDateToCompare;
+}
 
-// console.log(isAfter(new Date(1989, 6, 10), new Date(1987, 1, 11))); // true
-// console.log(isAfter(1648636135000, 1648549735000)); // true
-// console.log(isAfter(1648549735000, 1648636135000)); // false
-// console.log(isAfter(1648549735000, "dsasasd")); // Error
+console.log(isAfter(new Date(1989, 6, 10), new Date(1987, 1, 11))); // true
+console.log(isAfter(1648636135000, 1648549735000)); // true
+console.log(isAfter(1648549735000, 1648636135000)); // false
+console.log(isAfter(1648549735000, "dsasasd")); // Error
